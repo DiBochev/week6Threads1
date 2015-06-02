1fbc6c55bc23d39b2deaf2e64c0b087bf28f3f04
fgg12 <fgg12@FGG>
2015-06-02 23:14:29 +0300

    a

---
+++ src/Problem2/BlockingQueueMain.java
@@ -0,0 +1,16 @@
+package Problem2;
+
+
+public class BlockingQueueMain {
+
+    public static void main(String[] args) {
+        BlockingQueue<Integer> queue = new BlockingQueue<Integer>();
+        Integer number = 10;
+        BlockingQueuePoller<Integer> poller = new BlockingQueuePoller<Integer>(queue);
+        BlockingQueueAdder<Integer> adder = new BlockingQueueAdder<Integer>(queue, number);
+        
+        poller.start();
+        adder.start();
+    }
+
+}
---
+++ src/Problem2/BlockingQueuePoller.java
@@ -0,0 +1,14 @@
+package Problem2;
+
+
+public class BlockingQueuePoller<E> extends Thread {
+    private BlockingQueue<E> queue;
+    public BlockingQueuePoller(BlockingQueue<E> queue) {
+        this.queue = queue;
+    }
+    
+    public void run() {
+        E element = this.queue.poll();
+        System.out.println(element);
+    }
+}
